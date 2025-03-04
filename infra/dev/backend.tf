dc764b6f141323095231f2da335bc0e032e9e8f0
Rogelio Cisternas Vera <rogelio.cisterna@correoaiep.cl>
2025-03-04 13:50:05 -0300

    new tests

--- infra/dev/backend.tf
+++ infra/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "node-ts-ci-cd-github-poc"
+    bucket         = "node-ts-ci-cd-github-poc-external-modules"
     key            = "state/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
-    dynamodb_table = "node-ts-ci-cd-github-poc"
+    dynamodb_table = "node-ts-ci-cd-github-poc-external-modules"
   }
 }
